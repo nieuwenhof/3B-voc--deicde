734ebc178b8bf8204f8137bc829b42e54ae2f95d
Lauanny Nieuwenhoff Oliveira <126528019+nieuwenhof@users.noreply.github.com>
2024-06-20 08:30:01 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+const caixaPrincipal= document.querySelector('.caixa-principal');
+const caixaPerguntas= document.querySelector('.caixa-perguntas');
+const caixaAlternativas= document.querySelector('.caixa-alternativas');
+const caixaResultado= document.querySelector('.caixa-resultado');
+const textoResultado= document.querySelector('.texto-resultado');
+
+const perguntas = [
+    {
+        enunciado: "Enunciado 01",
+        alternativas: [
+            "alternativa 1",
+            "alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Enunciado 01",
+        alternativas: [
+            "alternativa 1",
+            "alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Enunciado 01",
+        alternativas: [
+            "alternativa 1",
+            "alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Enunciado 01",
+        alternativas: [
+            "alternativa 1",
+            "alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Enunciado 01",
+        alternativas: [
+            "alternativa 1",
+            "alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Enunciado 01",
+        alternativas: [
+            "alternativa 1",
+            "alternativa 2"
+        ]
+    },
+]
