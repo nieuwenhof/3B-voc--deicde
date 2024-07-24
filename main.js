3f6e855f85fb0cbfe235f5bb8c460671c58e2f3b
Lauanny Nieuwenhoff Oliveira <126528019+nieuwenhof@users.noreply.github.com>
2024-07-24 08:46:50 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -40,11 +40,22 @@ const perguntas = [
             "alternativa 2"
         ]
     },
-    {
-        enunciado: "Enunciado 01",
-        alternativas: [
-            "alternativa 1",
-            "alternativa 2"
-        ]
-    },
 ]
+let atual = 0;
+let perguntaAtual;
+
+function mostrarPerguntas(){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostrarAlternativas();
+}
+
+function mostrarAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+mostrarPerguntas();
