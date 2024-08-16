fef6e42ed059c06b60c3292c7105ed43ec77aefc
Lauanny Nieuwenhoff Oliveira <126528019+nieuwenhof@users.noreply.github.com>
2024-08-15 21:18:10 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -10,71 +10,71 @@ const perguntas = [
         alternativas: [
             {
                 texto:"Sou afavor da legalização pois todas mulheres deveriam ter direito sobre seu corpo e se quer gerar",
-                afirmacao: "Eu acho que todas mulheres deveriam sim ter controle sobre o que faz com seu corpo, se quer gerar ou não, esta mulher não deveria passar a ser julgada por sua escolha"
+                afirmacao: "Eu acho que todas mulheres deveriam sim ter controle sobre o que faz com seu corpo, se quer gerar ou não, esta mulher não deveria passar a ser julgada por sua escolha."
             },
             {
                 texto:"Não sou afavor da legalização pois muitas mulheres se submetem a fazer relação sexual mesmo sabendo da possibilidade de uma gestação.",
-                afirmacao: "Eu não sou afavor da legalização pois existem metodos contraceptivos para evitar uma gravidez s"
+                afirmacao: "Eu não sou afavor da legalização pois existem metodos contraceptivos para evitar uma gravidez e pessoas que se submetem fazer relação sem preservativo não deveria ter responsabilidade em acabar com uma vida."
             }
             
             
         ]
     },
     {
-        enunciado: "Enunciado 02",
+        enunciado: "No Brasil, o debate enfrenta o conservadorismo e limitações da lei ja que somente é permitido em apenas 3 situações. Mas nos últimos tempo esta sendo levantando muito em pauta a condenação para mulheres que abortam em caso de estrupo. Você é afavor da legalização do aborto em caso de violência?",
         alternativas: [
             {
-                texto:"alternativa 3",
-                afirmacao: "resultado 3"
+                texto:"Sou afavor em caso de estrupo pois a mulher já passou por uma violentação e deve ter direito de escolha.",
+                afirmacao: "Quando ocorre casos de estrupo sou afavor do aborto onde a mulher já tem que lidar com os transtornos da violação e tem que ser direito dela se ela quer ou não gerar e demais ser condenada por sua escolha é algo sem cabimento."
             },
                 {
-                    texto:"alternativa 4",
-                    afirmacao: "resultado 4"
+                    texto:"Não sou afavor do aborto pois o feto não tem culpa sobre o que ocorreu e quem acaba interrompendo deveria sim ser condenado",
+                    afirmacao: "Eu não sou afavor do aborto mesmo em caso de estrupo pois o feto não teve culpa sobre o que ocorreu e acho sim que a mulher que interrompe uma gestação deve ser condenada"
                 }
             
             
         ]
     },
     {
-        enunciado: "Enunciado 03",
+        enunciado: "O grande problema do aborto clandestino esta na realizaçção de procedimentos invasores e sem segurança mas para pessoas com boas condições conseguem achar uma 'clínica top' enquanto mulheres de baixa renda morrem constantemente. Você acha que esta diferença social iria ocorer se fosse liberado a legalização do aborto?",
         alternativas: [
             {
-                texto:"alternativa 5",
-                afirmacao:"resultado 5"
+                texto:"Sou afavor que seja liberado e com isso a taxa de morte de mulheres que tentam o aborto cairiam porque teriam oportunidade de acesso com boas condições.",
+                afirmacao:"Com a liberação do aborto seria uma causa pública isso ajudaria para mulheres terem segurança sobre suas escolhas."
             },
             {
-                texto:"alternativa 6",
-                afirmacao:"resultado 6"
+                texto:"Não sou afavor e acho isso um absurdo pois se fez filho tem obrigação de criar e cuidar.",
+                afirmacao:"Sou contra pois defendo a proteção do direito a vida mesmo de um feto e sendo iberado muits mulheres estriam violando os direito de vida deste feto pois cada ser humano tem seu valor e direito a vida."
             }
             
             
         ]
     },
     {
-        enunciado: "Enunciado 04",
+        enunciado: "O aborto espontâneo e comum entre as mulheres e a grande taxa sempre são mulheres que perdem o bebê antes da vigésima semana e isso pode causar dor física eemocional e muitos acreditam que isso ocorre por negligência da gestante, Você acredita nisso?",
         alternativas: [
             {
-                texto:"alternativa 7",
-                afirmacao:"resultado 7"
+                texto:"Acredito que a mulher não tem culpa quando ocorre aborto espontâneo",
+                afirmacao:"Em casos de abortos espontâneos a mulher não deveria ser acusada como responsável pois pode ocorrer por diversos motivos como problemas no feto ou envolvendo problemas na saúde da gestante. "
             },
             {
-                texto:"alternativa 8",
-                afirmacao:"resultado 8"
+                texto:"Acredito que seja sim negligência da gestante",
+                afirmacao:"È negliência da mulher pois deveria ter tomaado sim devidos cuidados sabendo quje carrega uma vida dentro de si"
             }
             
-            
+        
         ]
     },
     {
-        enunciado: "Enunciado 05",
+        enunciado: "Depois de todos esses trechos onde o aborto esta envolvido em várias situações Quais das situações abaixo você seria afavor?",
         alternativas: [
         {
-            texto:"alternativa 9",
-            afirmacao:"resultado 9"
+            texto:"Sou afavor do aborto em qualquer situação",
+            afirmacao:"O aborto deveria ser liberado em todas estas situações, acredito que a mulher deve controlar sobre o que quer ou não fazer com seu proprio corpo"
         },
         {
-            texto:"alternativa 10",
-            afirmacao:"resultado 10"
+            texto:"Ainda continuo sendo contra o aborto somente em alguns casos como o de estrupo",
+            afirmacao:"Sou contra o aborto e qualquer situação somente envolvend estrupos pois além disso todos que se relacionam sabe que existe a possibilidade de engravidar"
         }
            
             
